0ed93a96344d759c26895a44b442846cf3cf7c34
Md_Ashiqur_Rahman <53116858+Md-AshiqurRahman@users.noreply.github.com>
2022-09-01 01:29:40 +0600

    form validation

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import style from './CreateTodo.module.css';
 
@@ -8,34 +8,39 @@ const CreateTodo = (props) => {
         desc: ''
     });
 
-    const {title, desc} = todo;
+    const { title, desc } = todo;
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.onNewTodo(todo);
-        setTodo({title:'', desc:''})
+        if (!title || !desc) {
+            alert('Please filled up the form');
+        } else {
+            props.onNewTodo(todo);
+            setTodo({ title: '', desc: '' })
+        }
+
     }
 
     const eventHandler = (e) => {
         setTodo((oldTodo) => {
-            return {...oldTodo, [e.target.name] : e.target.value}
+            return { ...oldTodo, [e.target.name]: e.target.value }
         })
     }
 
     return (
-    <form className={style.form} onSubmit={handleSubmit}>
-        <div className={style["form-field"]}>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" value={title} onChange={eventHandler}></input>
-        </div>
+        <form className={style.form} onSubmit={handleSubmit}>
+            <div className={style["form-field"]}>
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" name="title" value={title} onChange={eventHandler}></input>
+            </div>
 
-        <div className={style["form-field"]}>
-            <label htmlFor="desc">Description</label>
-            <textarea type="text" id="desc" name="desc" value={desc} onChange={eventHandler}></textarea>
-        </div>
-        <button type="submit">Add Todo</button>
-    </form>
-  )
+            <div className={style["form-field"]}>
+                <label htmlFor="desc">Description</label>
+                <textarea type="text" id="desc" name="desc" value={desc} onChange={eventHandler}></textarea>
+            </div>
+            <button type="submit" disabled={!(title && desc)}>Add Todo</button>
+        </form>
+    )
 }
 
 export default CreateTodo;
